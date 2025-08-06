2c78e4fb04666ee0a76e93421a452dc8b866976a
Abdulrahman Alqawadri <A.Alqawadri@eska.com>
2025-08-06 17:28:50 +0300

    upload files

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,59 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using API.Error;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            // Filters out known noise like .well-known/appspecific
+            var path = context.Request.Path.ToString();
+            if (path.Contains("/.well-known/appspecific"))
+            {
+                return;
+            }
+
+            string body = "";
+
+            try
+            {
+                context.Request.EnableBuffering();
+
+                context.Request.Body.Position = 0;
+
+                using var reader = new StreamReader(
+                    context.Request.Body, Encoding.UTF8, leaveOpen: true);
+
+                body = await reader.ReadToEndAsync();
+
+                context.Request.Body.Position = 0; // Reset for next component
+            }
+            catch (Exception readEx)
+            {
+                body = $"[Unable to read body: {readEx.Message}]";
+            }
+
+            var customMessage = ex.Message + $"\n\n[Request Body]:\n" + body;
+            
+            var wrappedEx = new Exception(customMessage, ex);
+
+            await ElmahCore.ElmahExtensions.RaiseError(context, wrappedEx);
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,122 @@
+using API.Extensions;
+using API.Middleware;
+using API.SignalR;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Exceptions;
+using ElmahCore.Mvc;
+using ElmahCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+builder.Services.AddIdentityService(builder.Configuration);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddCors();
+
+builder.Services.AddElmah<ElmahCore.XmlFileErrorLog>(options =>
+{
+    options.LogPath = "~/logs"; // or use database provider
+
+    // options.OnPermissionCheck = context => context.User.HasClaim("Permission", "CanViewElmah");
+});
+
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug() 
+        .WriteTo.Console() 
+        .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day) 
+        .CreateLogger();
+
+// builder.Host.UseSerilog();
+
+// builder.Logging.ClearProviders();
+// builder.Logging.AddConsole();
+// builder.Logging.SetMinimumLevel(LogLevel.Debug); // 
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<DataContext>();
+        var userManager = services.GetRequiredService<UserManager<AppUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+        var unitOfWork = services.GetRequiredService<IUnitOfWork>();
+
+        await context.Database.MigrateAsync();
+        await Seeder.SeedUsers(userManager, roleManager, unitOfWork);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+
+        logger.LogError(ex, "An error occured while seeding database");
+    }
+}
+
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("https://localhost:4200")
+);
+
+app.UseMiddleware<RequestLocalizationMiddleware>();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+// app.Use(async (context, next) =>
+// {
+//     try
+//     {
+//         await next();
+//     }
+//     catch
+//     {
+//         // Let ExceptionMiddleware handle it
+//         throw;
+//     }
+
+//     var path = context.Request.Path.ToString();
+
+//     // Exclude known devtools or browser-specific noise
+//     if (context.Response.StatusCode == 404 && path.Contains("/.well-known/appspecific"))
+//     {
+//         // Prevent Elmah from logging this
+//         context.Items["__elmah_disable"] = true;
+//     }
+// });
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseElmah();
+
+app.MapHub<PresenceHub>("hubs/presence");
+
+app.MapHub<TwoPersonsMessageHub>("hubs/dual-messaging");
+
+app.MapControllers();
+
+app.Run();
